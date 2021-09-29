989a5c82bbcbf54bbdd8226ba652e13c1bb3d5b9
m0hammadr3za <mohammad.reza.gholami.dev@gmail.com>
2021-09-29 19:30:58 +0330

    Responsive navbar and project's basic setup added

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./styles/App.css";
+
+function App() {
+    return (
+        <>
+            <Header />
+            <main>{/*  */}</main>
+            <Footer />
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import "../styles/Navbar.css";
+
+interface menuInterface {
+    isOpen: boolean;
+    startOpenAnimation: boolean;
+    startCloseAnimation: boolean;
+}
+
+export default function Navbar() {
+    const [menu, setMenu] = useState<menuInterface>({
+        isOpen: false,
+        startOpenAnimation: false,
+        startCloseAnimation: false,
+    });
+
+    function openMenu(): void {
+        // Add the menu
+        setMenu((prevState) => ({
+            ...prevState,
+            isOpen: true,
+        }));
+
+        // Start the opening animation
+        setTimeout(() =>
+            setMenu((prevState) => ({
+                ...prevState,
+                startOpenAnimation: true,
+            }))
+        );
+    }
+
+    function closeMenu(): void {
+        // Start the closing animation
+        setMenu((prevState) => ({
+            ...prevState,
+            startCloseAnimation: true,
+        }));
+
+        // Remove the menu and reset the state
+        setTimeout(() => {
+            setMenu({
+                isOpen: false,
+                startOpenAnimation: false,
+                startCloseAnimation: false,
+            });
+        }, 500);
+    }
+
+    return (
+        <nav className="navbar">
+            <a href="#" className="navbar__brand">
+                CO<span>MPLEXITY</span>
+            </a>
+
+            <div className="navbar__menu-icon" onClick={openMenu}>
+                <div className="menu-icon__line" />
+                <div className="menu-icon__line" />
+                <div className="menu-icon__line" />
+            </div>
+
+            <div
+                className={`navbar__menu ${menu.isOpen ? "navbar__menu--show" : ""} ${
+                    menu.startOpenAnimation ? "navbar__menu--animate-in" : ""
+                } ${menu.startCloseAnimation ? "navbar__menu--animate-out" : ""}`}
+            >
+                <div className="menu__close-menu" onClick={closeMenu}>
+                    <div className="close-icon">
+                        <div className="close-icon__line" />
+                        <div className="close-icon__line" />
+                    </div>
+
+                    <p className="close-menu__text">Close menu</p>
+                </div>
+
+                <ul className="navigation">
+                    <li className="navigation__link">
+                        <a href="#">Home</a>
+                    </li>
+
+                    <li className="navigation__link">
+                        <a href="#">About</a>
+                    </li>
+
+                    <li className="navigation__link">
+                        <a href="#">Contact</a>
+                    </li>
+
+                    <li className="navigation__link navigation__link--button">
+                        <a href="#">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
